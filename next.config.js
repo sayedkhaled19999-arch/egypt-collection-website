3b9dc7534d79be925cdd7241ce03ae3642ee59a3
Sayed <sayed@example.com>
2026-01-06 15:32:03 +0200

    تعديل

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*', // كل الروابط
+        has: [{ type: 'host', value: 'www.egyptcollections.com' }], // لو فيها www
+        destination: 'https://egyptcollections.com/:path*', // حولها للنسخة بدون www
+        permanent: true, // redirect دائم 301
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
